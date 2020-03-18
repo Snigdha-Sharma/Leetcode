c1220b85b2b53f970ac82f2f30db33a75ff34e94
Snigdha Sharma <46266633+Snigdha-Sharma@users.noreply.github.com>
2020-03-18 23:12:25 +0530

    Add files via upload

---
+++ Easy/Majority Element.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public:
+    int majorityElement(vector<int>& nums) 
+    {
+        int maj_index=0,count=1;
+        for (int i=1;i<nums.size();i++)
+        {
+            if (nums[i]==nums[maj_index])
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+            if (count==0)
+            {
+                maj_index=i;
+                count=1;
+            }
+        }
+        return nums[maj_index];
+    }
+};
