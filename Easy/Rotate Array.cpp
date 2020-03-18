231675c07414c9a0cba2d1da24fd7f3350c55251
Snigdha Sharma <46266633+Snigdha-Sharma@users.noreply.github.com>
2020-03-19 00:22:11 +0530

    Add files via upload

---
+++ Easy/Rotate Array.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        if (k>nums.size())
+        {
+            k=k%nums.size();
+        }
+        if (k==0)
+        {
+            return;
+        }
+        int temp[k];
+        for (int i=nums.size()-k,j=0;i<nums.size();i++,j++)
+        {
+            temp[j]=nums[i];
+        }
+        for (int i=nums.size()-1;i>=k;i--)
+        {
+            nums[i]=nums[i-k];
+        }
+        for (int i=0;i<k;i++)
+        {
+            nums[i]=temp[i];
+        }
+    }
+};
